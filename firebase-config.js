9597aca3955aa9cf940f846e66a16ef841ab45f0
cscanavar4443 <cscanavar885@hotmail.com>
2026-03-09 16:25:05 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase, ref, push, onChildAdded } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+const cfg = {
+  apiKey: "AIzaSyBpCLRiS8pHlTgvVmNLH92u3VszvT25xPs",
+  databaseURL: "https://trchat-7bc26-default-rtdb.firebaseio.com",
+  projectId: "trchat-7bc26"
+};
+
+const app = initializeApp(cfg);
+const db = getDatabase(app);
+const messagesRef = ref(db, 'messages');
+
+export { db, messagesRef, push, onChildAdded };
